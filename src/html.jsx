cdf402299fdc541480fb5ff220a861f658e15b24
Chris Atkinson <chrisacky@gmail.com>
2020-05-29 09:36:29 +0100

    Fix the flash of large font.

--- src/html.jsx
+++ src/html.jsx
@@ -2,7 +2,12 @@
 /* eslint import/extensions:"off" */
 /* eslint global-require:"off" */
 import React from 'react';
-
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
 export default class HTML extends React.Component {
 
   render() {
